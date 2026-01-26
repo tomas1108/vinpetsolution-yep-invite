63b83f23223bdb07c9d91f04081e71ff89e5e449
Ninh Hoang Khai <nhkkhaii@gmail.com>
2026-01-26 21:58:37 +0700

    feat: Add Korean language support and new name entries to NameDetailPage

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -11,6 +11,7 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
     // State
     const [isOpen, setIsOpen] = useState(false)
     const nameData = nameConstants.find(item => item.key === name)
+    const isKorean = name === "parkseryun"
     const [isMobile, setIsMobile] = useState(false)
     useEffect(() => {
         const handleResize = () => {
@@ -120,7 +121,9 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
                         </div>
 
                         <div className="absolute lg:top-[calc(365/820*100%)] top-[calc(308/932*100%)] flex flex-col gap-1 items-center justify-center left-1/2 -translate-x-1/2">
-                            <p className="lg:text-[calc(18/1536*100vw)] text-[calc(18/430*100vw)] tracking-widest">Trân trọng kính mời</p>
+                            <p className="lg:text-[calc(18/1536*100vw)] text-[calc(18/430*100vw)] tracking-widest">
+                                {isKorean ? "정중히 초대합니다" : "Trân trọng kính mời"}
+                            </p>
                             <h2 className="text-[calc(20/430*100vw)] lg:text-[calc(30/1536*100vw)] font-bold tracking-wide uppercase">
                                 {nameData.value}
                             </h2>
@@ -128,18 +131,22 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
 
 
                         {/* Invitation Content */}
-                        <div className="absolute top-[calc(395/932*100%)] w-full gap-[calc(12/932*100vh)] lg:gap-4 lg:top-[calc(466/820*100%)] lg:w-[calc(415/1536*100%)] lg:h-[calc(170/820*100%)] left-1/2 -translate-x-1/2 text-center text-white flex flex-col items-center justify-center">
+                        <div className="absolute top-[calc(395/932*100%)] w-full gap-[calc(12/932*100vh)] lg:gap-4 lg:top-[calc(466/820*100%)]  left-1/2 -translate-x-1/2 text-center text-white flex flex-col items-center justify-center">
                             <div className="grid grid-cols-3 h-full items-center gap-2 lg:gap-4">
-                                <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)] italic px-6 md:px-6 py-2 border-y-2 border-white">Thứ 7</span>
+                                <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)] italic px-6 md:px-6 py-2 border-y-2 border-white">
+                                    {isKorean ? "토요일" : "Thứ 7"}
+                                </span>
                                 <div className="flex h-full items-center justify-center">
                                     <span className="text-[calc(112/430*100vw)] lg:text-[calc(128/1536*100vw)] font-light leading-[0.7] translate-y-[0.1em]">5</span>
                                 </div>
                                 <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)] px-6 md:px-6 py-2 border-y-2 border-white">18:30</span>
                             </div>
-                            <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)]">Tháng 2</span>
+                            <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)]">
+                                {isKorean ? "2월" : "Tháng 2"}
+                            </span>
                         </div>
                         <div className="lg:text-[calc(18/1536*100vw)] whitespace-nowrap text-[calc(16/430*100vw)] absolute top-[calc(540/932*100%)] lg:top-[calc(655/820*100%)] left-1/2 -translate-x-1/2 text-center text-white">
-                            Địa điểm: 424 Nguyễn Thái Sơn, P An Nhơn, TPHCM
+                            {isKorean ? "장소: 424 Nguyễn Thái Sơn, P An Nhơn, TPHCM" : "Địa điểm: 424 Nguyễn Thái Sơn, P An Nhơn, TPHCM"}
                         </div>
                     </motion.div>
                 </motion.div>
