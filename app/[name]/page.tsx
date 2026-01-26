a9b085e4728d35cfba52b4dca340e732d5e39ef4
Ninh Hoang Khai <nhkkhaii@gmail.com>
2026-01-26 21:17:18 +0700

    feat: Enhance NameDetailPage with responsive design and mobile background image

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { nameConstants } from "@/constants/nameConstants";
@@ -7,8 +7,18 @@ import { notFound } from "next/navigation";
 
 const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
     const { name } = use(params)
+
+    // State
     const [isOpen, setIsOpen] = useState(false)
     const nameData = nameConstants.find(item => item.key === name)
+    const [isMobile, setIsMobile] = useState(false)
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
     if (!nameData) {
         notFound();
@@ -44,12 +54,22 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
                         className="relative w-full h-full overflow-hidden"
                     >
 
+                        {/* Desktop Background */}
                         <Image
                             src="/images/bg/bg.png"
                             alt="Popup Background"
                             width={1536}
                             height={820}
-                            className="object-cover w-full h-full aspect-1536/820"
+                            className="hidden md:block object-cover w-full h-full"
+                            priority
+                        />
+                        {/* Mobile Background */}
+                        <Image
+                            src="/images/bg/bgPortrait.png"
+                            alt="Popup Background Mobile"
+                            width={430}
+                            height={932}
+                            className="block md:hidden object-cover w-full h-full"
                             priority
                         />
 
@@ -80,7 +100,7 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
                             />
                         </div> */}
 
-                        <div className="absolute top-[calc(50/820*100%)] w-[calc(200/1536*100%)] h-[calc(56/820*100%)] left-1/2 -translate-x-1/2">
+                        <div className="absolute w-[calc(132/430*100%)] h-[calc(34/932*100%)] top-[calc(147/932*100%)] lg:top-[calc(50/820*100%)] lg:w-[calc(200/1536*100%)] lg:h-[calc(56/820*100%)] left-1/2 -translate-x-1/2">
                             <Image
                                 src="/images/logo/logovinpet-02.webp"
                                 alt="Logo"
@@ -89,7 +109,7 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
                                 className="aspect-200/56 w-full h-full object-contain"
                             />
                         </div>
-                        <div className="absolute w-[calc(621/1536*100%)] h-[calc(193/820*100%)] top-[calc(161/820*100%)] left-1/2 -translate-x-1/2">
+                        <div className="absolute w-[calc(321/430*100%)] h-[calc(100/932*100%)] top-[calc(192/932*100%)] lg:w-[calc(621/1536*100%)] lg:h-[calc(193/820*100%)] lg:top-[calc(161/820*100%)] left-1/2 -translate-x-1/2">
                             <Image
                                 src="/images/bg/YEP.png"
                                 alt="Logo"
@@ -99,26 +119,26 @@ const NameDetailPage = ({ params }: { params: Promise<{ name: string }> }) => {
                             />
                         </div>
 
-                        <div className="absolute top-[calc(365/820*100%)] flex flex-col gap-1 items-center justify-center left-1/2 -translate-x-1/2">
-                            <p className="text-[calc(18/1536*100vw)] tracking-widest">Trân trọng kính mời</p>
-                            <h2 className="text-2xl md:text-[calc(30/1536*100vw)] font-bold tracking-wide uppercase">
+                        <div className="absolute lg:top-[calc(365/820*100%)] top-[calc(308/932*100%)] flex flex-col gap-1 items-center justify-center left-1/2 -translate-x-1/2">
+                            <p className="lg:text-[calc(18/1536*100vw)] text-[calc(18/430*100vw)] tracking-widest">Trân trọng kính mời</p>
+                            <h2 className="text-[calc(20/430*100vw)] lg:text-[calc(30/1536*100vw)] font-bold tracking-wide uppercase">
                                 {nameData.value}
                             </h2>
                         </div>
 
 
                         {/* Invitation Content */}
-                        <div className="absolute top-[calc(466/820*100%)] w-[calc(415/1536*100%)] h-[calc(170/820*100%)] left-1/2 -translate-x-1/2 text-center text-white flex flex-col items-center justify-center">
-                            <div className="grid grid-cols-3 h-full items-center gap-4">
-                                <span className="text-2xl md:text-[calc(30/1536*100vw)] italic px-6 md:px-6 py-2 border-y-2 border-white">Thứ 7</span>
+                        <div className="absolute top-[calc(395/932*100%)] w-full gap-[calc(12/932*100vh)] lg:gap-4 lg:top-[calc(466/820*100%)] lg:w-[calc(415/1536*100%)] lg:h-[calc(170/820*100%)] left-1/2 -translate-x-1/2 text-center text-white flex flex-col items-center justify-center">
+                            <div className="grid grid-cols-3 h-full items-center gap-2 lg:gap-4">
+                                <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)] italic px-6 md:px-6 py-2 border-y-2 border-white">Thứ 7</span>
                                 <div className="flex h-full items-center justify-center">
-                                    <span className="text-6xl md:text-[calc(128/1536*100vw)] font-light leading-[0.7] translate-y-[0.1em]">5</span>
+                                    <span className="text-[calc(112/430*100vw)] lg:text-[calc(128/1536*100vw)] font-light leading-[0.7] translate-y-[0.1em]">5</span>
                                 </div>
-                                <span className="text-2xl md:text-[calc(30/1536*100vw)] px-6 md:px-6 py-2 border-y-2 border-white">18:30</span>
+                                <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)] px-6 md:px-6 py-2 border-y-2 border-white">18:30</span>
                             </div>
-                            <span className="text-2xl md:text-[calc(30/1536*100vw)]">Tháng 2</span>
+                            <span className="text-[calc(26/430*100vw)] lg:text-[calc(30/1536*100vw)]">Tháng 2</span>
                         </div>
-                        <div className="text-[calc(18/1536*100vw)] absolute top-[calc(655/820*100%)] left-1/2 -translate-x-1/2 text-center text-white">
+                        <div className="lg:text-[calc(18/1536*100vw)] whitespace-nowrap text-[calc(16/430*100vw)] absolute top-[calc(540/932*100%)] lg:top-[calc(655/820*100%)] left-1/2 -translate-x-1/2 text-center text-white">
                             Địa điểm: 424 Nguyễn Thái Sơn, P An Nhơn, TPHCM
                         </div>
                     </motion.div>
